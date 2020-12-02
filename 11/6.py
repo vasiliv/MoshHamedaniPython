c03f3dc181e4295681927d093563af06a07e2007
grandiosa iva <ggo24@yahoo.com>
2020-12-02 00:23:29 -0800

    lecture_11_6

---
+++ 11/6.py
@@ -0,0 +1,16 @@
+from twilio.rest import Client
+
+account_sid = "AC247a2911850d75b3b3bb8f78c105c8f3"
+auth_token = "b2d8dc8624df1a4f2e131593939bb844"
+
+client = Client(account_sid,auth_token)
+
+# interesting attributes
+# client.calls
+# client.chat
+call = client.messages.create(
+    to="+995595604040",
+    from_="+15155199793",
+    body="Test"
+)
+
